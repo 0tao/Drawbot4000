258babe0aaebe7a9f43561dedaf0e714ace3e53d
grantstewart <grant.c.stewart@gmail.com>
2016-01-11 20:46:42 +1100

    Commented out main bot - simple drawing to work out XY coords
    Commented out main bot, working on simple lines to figure out the maths
    of X Y boundaries

--- Drawbot4000.ino
+++ Drawbot4000.ino
@@ -6,13 +6,13 @@
 #define MAX_DISTANCE 300 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
 int ledPin = 14;  //define the pin that the LED is on
-int motorSpeed = 4;  //the speed of the motors  (6-7 is about the max speed with a weight  - 2-3 max without weight)
+int motorSpeed = 1;  //the speed of the motors  (6-7 is about the max speed with a weight  - 2-3 max without weight)
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
 int upperLimit = 1;  //the top of the drawing space
 int lowerLimit = 1300; //the bottom centre of the drawing space
-int L = 1299; // Left motor string length - within the defined space above
-int R = 1299; // Right Motor string length - within the defined space above
+int L = -1; // Left motor string length - within the defined space above (its -1 becaues i push it + 1 in the setup to engage the motors straight away)
+int R = -1; // Right Motor string length - within the defined space above (its -1 becaues i push it + 1 in the setup to engage the motors straight away)
 
 
 ///////// the variables beow control how the lines are drawn
@@ -36,7 +36,9 @@ void setup() {
   RM.setSpeed(motorSpeed); //initiate right motor speed
   delay(2000);
   RM.step(1, BACKWARD, INTERLEAVE); //engage right motor
-  LM.step(1, BACKWARD, INTERLEAVE); //engage left motor
+  R+=1;
+  LM.step(1, FORWARD, INTERLEAVE); //engage left motor
+  L+=1;
   pinMode(ledPin, OUTPUT);  
   delay(4000);  //wait 4 seconds, then start the bot going
 }
@@ -45,58 +47,82 @@ void setup() {
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void loop() {
-  
-  
+
+
+  int choice = int(random(10));  
+  LM.step(choice, FORWARD, INTERLEAVE);
+  L+=choice;
+  Serial.println(L);
+  while(L>=200){
+    LM.step(1, BACKWARD, INTERLEAVE);
+    L-=1;
+    Serial.println(L);
+  }
+
+  int choice2 = int(random(10)); 
+  LM.step(choice2, BACKWARD, INTERLEAVE);
+  L-=choice2;
+  Serial.println(L);
+  while(L<=0){
+    LM.step(1, FORWARD, INTERLEAVE);
+    L+=1;
+    Serial.println(L);
+  }
+
+
+
   /*
   ////////////////////////////////////////////SENSOR INPUT//////////////////////////////////////////////////////////////////////////////////////
-  delay(50);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
-  Serial.print("Ping: ");
-  Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
-  Serial.println("cm");
-  while(sonar.ping_cm() <10){  //while the sensor is reading 'not much' set the motor speed to 0 and the LED to off.
-    digitalWrite(ledPin, LOW); 
-    LM.setSpeed(0);
-    RM.setSpeed(0);
+   delay(50);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+   Serial.print("Ping: ");
+   Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
+   Serial.println("cm");
+   while(sonar.ping_cm() <10){  //while the sensor is reading 'not much' set the motor speed to 0 and the LED to off.
+   digitalWrite(ledPin, LOW); 
+   LM.setSpeed(0);
+   RM.setSpeed(0);
+   
+   }
+   while(sonar.ping_cm() >10){ //while the sensor is reading HIGH, set motors moving and turn on the LED
+   digitalWrite(ledPin, HIGH); 
+   LM.setSpeed(4);
+   RM.setSpeed(4);
+   }
+   
+   */
 
-  }
-  while(sonar.ping_cm() >10){ //while the sensor is reading HIGH, set motors moving and turn on the LED
-    digitalWrite(ledPin, HIGH); 
-    LM.setSpeed(4);
-    RM.setSpeed(4);
-  }
 
-  */
-  
-
-
+  /*
 /////////////////////////////////////////////////////CHOOSE DRAWING STYLE BASED ON LINE LENGTH AND LINE GAP VARIABLES //////////////////////////////////////////////////////////////////////////////////////////
-  int randoChoice = int(random(100));  // this defines how long the long back and forth lines are
-  if (randoChoice<4){
-    randoLength = 500;
-  }
-  else{
-    randoLength = 30;
-  }
-
-
-  int lineGapChoice = int(random(100));  // this defines the amound of gap separating the long back and forth lines
-  if (lineGapChoice<6){
-    lineGap = int(random(100));
-  } 
-  else {
-    lineGap = 5;
-  }
-  
-  ////////////////////////////////////////////////////////////MOVE THE BOT WHILE TESTING BOUNDARIES////////////////////////////////////////////////////////////////////////////////// 
-  LeftSideUpLineShapes();
-  RightSideDownLineShapes();
-  LeftSideDownLineShapes();
-  RightSideUpLineShapes();
-  
- 
-  //testBoundaries();
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+   int randoChoice = int(random(100));  // this defines how long the long back and forth lines are
+   if (randoChoice<4){
+   randoLength = 500;
+   }
+   else{
+   randoLength = 30;
+   }
+   
+   
+   int lineGapChoice = int(random(100));  // this defines the amound of gap separating the long back and forth lines
+   if (lineGapChoice<6){
+   lineGap = int(random(100));
+   } 
+   else {
+   lineGap = 5;
+   }
+   
+   ////////////////////////////////////////////////////////////MOVE THE BOT WHILE TESTING BOUNDARIES////////////////////////////////////////////////////////////////////////////////// 
+   LeftSideUpLineShapes();
+   RightSideDownLineShapes();
+   LeftSideDownLineShapes();
+   RightSideUpLineShapes();
+   
+   
+   //testBoundaries();
+   ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+   */
 }
+
 
 
 
